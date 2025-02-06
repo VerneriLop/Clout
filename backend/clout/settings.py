a1c6d611010a9a5ed1f8e68a26b11625c54899f5
Verneri <verneri.lopperi@gmail.com>
2025-02-06 11:55:48 +0200

    docker for backend and database

--- backend/clout/settings.py
+++ backend/clout/settings.py
@@ -73,11 +73,11 @@ WSGI_APPLICATION = 'clout.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
