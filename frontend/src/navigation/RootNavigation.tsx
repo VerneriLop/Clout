2894a8c249f93d3bfdd0ee3ca10e5cd76772b818
Verneri <verneri.lopperi@gmail.com>
2025-02-26 13:51:09 +0200

    FIX: Navigation depends on authentication.

--- frontend/src/navigation/RootNavigation.tsx
+++ frontend/src/navigation/RootNavigation.tsx
@@ -4,10 +4,8 @@ import {Authenticated, NonAuthenticated} from './MainNavigation';
 import {RootState} from '../redux/store/store';
 
 export const RootNavigation = () => {
-  // Haetaan käyttäjän kirjautumistila Reduxista
   const user = useSelector((state: RootState) => state.user);
 
-  // Jos käyttäjä on kirjautunut, näytetään Authenticated näkymä, muuten NonAuthenticated
-  //return user.isAuthenticated ? <Authenticated /> : <NonAuthenticated />;
-  return <Authenticated />;
+  return user.isAuthenticated ? <Authenticated /> : <NonAuthenticated />;
+  //return <Authenticated />;
 };
