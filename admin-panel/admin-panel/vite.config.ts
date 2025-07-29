e27e1bbd9ed32a1f616b144f937d93b7edfcefb8
Verneri <verneri.lopperi@gmail.com>
2025-07-29 15:04:22 +0300

    Fix: Add middleware with cors changes. Add chromedevtoolsbypass in viteconfig

--- admin-panel/admin-panel/vite.config.ts
+++ admin-panel/admin-panel/vite.config.ts
@@ -2,7 +2,23 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import type { PluginOption } from "vite";
+
+function chromeDevToolsBypass(): PluginOption {
+  return {
+    name: "bypass-chrome-devtools-request",
+    configureServer(server) {
+      server.middlewares.use(
+        "/.well-known/appspecific/com.chrome.devtools.json",
+        (req, res) => {
+          res.statusCode = 204; // No Content
+          res.end();
+        },
+      );
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), chromeDevToolsBypass()],
 });
